6fe20ac7cebb145b8b54488ab9c7dc44da59e1d4
Olivier Levasseur <levasseuro.ol@gmail.com>
2016-11-10 10:23:56 -0500

    Modification de countries.h et countries.c
    -Il y avait une signature d'une fonction dans countries.h qui ne
    correspondait pas à celle de countries.c
    -Ajout d'une fonction qui retourne le code d'un pays

--- countries.h
+++ countries.h
@@ -5,6 +5,8 @@
 
 json_t* countries_getJsonObjectFromCountry(char * code, json_t* tabPays); 
 
+const char * countries_getCode(json_t *pays);
+
 const char * countries_getNomPays(json_t * pays);
  
 const char * countries_getCapitale(json_t * pays);
@@ -17,6 +19,6 @@ int countries_nbLangues(json_t * pays);
 
 json_t * countries_langues(json_t *pays); 
 
-json_t ** countries_paysSelonRegion(json_t *tabPays, char * region);
+json_t * countries_paysSelonRegion(json_t *tabPays, char * region);
 
 const char * countries_region(json_t *pays);
