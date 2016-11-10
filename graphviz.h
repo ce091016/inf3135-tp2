29ee50b7380dd4ba40ca238794b40320e3a56a13
Olivier Levasseur <levasseuro.ol@gmail.com>
2016-11-10 14:35:50 -0500

    Modification de graphviz.c et graphviz.h
    -Modification de la fonction ecrireUnPays: elle n'ouvre plus elle-même
    le fichier où elle doit écrire et elle n'écrit plus la partie du code
    permettant de commencer le fichier graphviz. De cette façon, elle pourra
    être appelée à répétition lorsqu'on veut écrire plus d'un fichier.
    -Ajout d'une fonction permettant de créer un fichier .dot pour un seul
    pays. Cette fonction appelle la fonction modifiée de la version
    antérieure de ce module.

--- graphviz.h
+++ graphviz.h
@@ -1,2 +1,7 @@
+void graphviz_ecrireUnSeulPays(int langues, int capitale, int frontieres, int flag, char * codePays, json_t *tabPays);
 
-void graphviz_ecrireUnPays(int langues, int capitale, int frontieres, int flag, char * codePays, json_t *tabPays); 
+void graphviz_ecrireUnPays(int langues, int capitale, int frontieres, int flag, char * codePays, json_t *tabPays, FILE * graphviz);
+
+
+
+
