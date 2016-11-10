1efe8f8db4feb1f93f79918808d1c8e6b2a0ed01
Jean-Lou <jeanlou601@hotmail.com>
2016-11-10 10:25:56 -0500

    Version brouillon du main.

---
+++ test.c
@@ -0,0 +1,61 @@
+#include<jansson.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include"input.h"
+#include"countries.h"
+
+#define TAILLE_MAX 12
+#define HELP 0
+#define OUTPUT_FORMAT 1
+#define OUTPUT_FILENAME 3
+#define SHOW_LANGUAGES 5
+#define SHOW_CAPITAL 6
+#define SHOW_BORDERS 7
+#define SHOW_FLAG
+#define COUNTRY 9
+#define REGION 11
+
+
+int main(int argc, char *argv[]){
+    json_error_t error;
+    json_t *root;
+    root = json_load_file("data/countries/countries.json",0,&error);
+    json_t *test;
+    json_t *value;
+    json_t *langues;
+    const char *chaine;
+    void *iter;
+    char *string;
+    char **rep = (char**)calloc(TAILLE_MAX + 1, sizeof(char*));
+    
+    input(argc, argv, rep);
+    
+    if(rep[COUNTRY] != NULL){
+        test = countries_getJsonObjectFromCountry(rep[COUNTRY + 1],root);
+        printf("Name : %s\n",countries_getNomPays(test));
+    }
+    if(rep[SHOW_LANGUAGES] != NULL){
+        printf("Langues: ");
+        countries_getLangues(test);
+    }
+    if(rep[SHOW_CAPITAL] != NULL){
+        printf("Capital : %s\n",countries_getCapitale(test));
+    
+    }
+    if(rep[SHOW_BORDERS] != NULL){
+    
+    }
+    if(rep[SHOW_FLAG] != NULL){
+    }
+    if()
+
+
+
+
+    free(rep);
+
+
+    return 0;
+}
