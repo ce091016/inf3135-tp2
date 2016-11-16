67052788e6c239acfe3a079f2c16d6b510b942ca
Olivier Levasseur <levasseuro.ol@gmail.com>
2016-11-16 11:53:48 -0500

    Modification de graphviz.c
    -La capitale du pays n'était jamais écrite dans le fichier (oubli de ma
    part)

--- src/graphviz.c
+++ src/graphviz.c
@@ -19,31 +19,9 @@
 #include <string.h>
 #include <ctype.h>
 
+#include "graphviz.h"
 #include "countries.h"
 #include "util.h"
-
-// Constantes
-// ----------
-
-#define DEBUTGRAPH "graph {\n"
-#define FINGRAPH "}\n"
-#define DEBUTPAYS "    " 
-#define DEBUTPAYS2 " [\n"
-#define FINPAYS "    ];\n"
-#define SHAPE "        shape = none\n"
-#define LABEL "        label = <<table border=\"0\" cellspacing=\"0\">\n"
-#define LABELFIN "        </table>>\n"
-#define TRTDIMAGE "            <tr><td align=\"center\" border=\"1\" fixedsize=\"true\" width=\"200\" height=\"100\">"
-#define TRTDIMAGE2 "<img src=\"data/countries/data/"
-#define TRTDIMAGE3 ".png\" scale=\"true\"/>"
-#define TRTDIMAGE4 "</td></tr>\n"
-#define TRTDNAME "            <tr><td align=\"left\" border=\"1\"><b>Name</b>: "
-#define TRTDCODE "            <tr><td align=\"left\" border=\"1\"><b>Code</b>: "
-#define TRTDCAPITAL "            <tr><td align=\"left\" border=\"1\"><b>Capital</b>: "
-#define TRTDLANGUAGE "            <tr><td align=\"left\" border=\"1\"><b>Language</b>: "
-#define TRTDBORDERS "            <tr><td align=\"left\" border=\"1\"><b>Borders</b>: " 
-#define FINTRTD "</td></tr>\n"
-
 
 // Implementation
 // --------------
@@ -60,6 +38,9 @@ void graphviz_ecrireUnPays(int langues, int capitale, int frontieres, int flag, 
     }
     fprintf(graphviz, "%s%s%s", TRTDNAME, countries_getNomPays(pays), FINTRTD);
     fprintf(graphviz, "%s%s%s", TRTDCODE, countries_getCode(pays), FINTRTD);
+    if (capitale == 1) {
+        fprintf(graphviz, "%s%s%s", TRTDCAPITAL, countries_getCapitale(pays), FINTRTD); 
+    }
     if (langues == 1) {
         json_t * langues = countries_langues(pays);
         fprintf(graphviz, "%s", TRTDLANGUAGE);
