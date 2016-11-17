291be0655557a2c13682b3e08eb65177c660b570
Olivier Levasseur <levasseuro.ol@gmail.com>
2016-11-17 09:51:16 -0500

    Modification de graphviz
    -Les constantes sont passées du fichier graphviz.c au fichier graphviz.h

--- src/graphviz.h
+++ src/graphviz.h
@@ -13,6 +13,28 @@
 * Email:           
 */
 
+// Constantes
+// ----------
+
+#define DEBUTGRAPH "graph {\n"
+#define FINGRAPH "}\n"
+#define DEBUTPAYS "    " 
+#define DEBUTPAYS2 " [\n"
+#define FINPAYS "    ];\n"
+#define SHAPE "        shape = none\n"
+#define LABEL "        label = <<table border=\"0\" cellspacing=\"0\">\n"
+#define LABELFIN "        </table>>\n"
+#define TRTDIMAGE "            <tr><td align=\"center\" border=\"1\" fixedsize=\"true\" width=\"200\" height=\"100\">"
+#define TRTDIMAGE2 "<img src=\"data/countries/data/"
+#define TRTDIMAGE3 ".png\" scale=\"true\"/>"
+#define TRTDIMAGE4 "</td></tr>\n"
+#define TRTDNAME "            <tr><td align=\"left\" border=\"1\"><b>Name</b>: "
+#define TRTDCODE "            <tr><td align=\"left\" border=\"1\"><b>Code</b>: "
+#define TRTDCAPITAL "            <tr><td align=\"left\" border=\"1\"><b>Capital</b>: "
+#define TRTDLANGUAGE "            <tr><td align=\"left\" border=\"1\"><b>Language</b>: "
+#define TRTDBORDERS "            <tr><td align=\"left\" border=\"1\"><b>Borders</b>: " 
+#define FINTRTD "</td></tr>\n"
+
 // Prototypes  
 // ---------- 
 
