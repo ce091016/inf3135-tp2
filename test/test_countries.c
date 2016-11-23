783cc0b974567c49a7405c1bd6227a9639cd4c3b
Jean-Lou <jeanlou601@hotmail.com>
2016-11-22 21:46:05 -0500

    Modification des tests fichiers.
    -Intégration dans test_countries.c
    -Modification de l'affichage

--- test/test_countries.c
+++ test/test_countries.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <jansson.h>
+#include <stdbool.h>
 
 #include "CUnit/Basic.h"
 #include "CUnit/CUnit.h"
 #include "../src/countries.h"
 #include "../src/util.h"
+
+bool dotFileTest(){
+    
+    bool passOrFail = false;
+    system("bin/tp2 --country can --output-format dot --output-filename test.dot");
+    FILE *file = fopen("test.dot","r");
+    if(file != NULL){
+        printf("Test 'dot' file réussi.\n");
+        passOrFail = true;
+        system("rm test.dot");
+    }else{
+        printf("Test 'dot' file échoué.\n");
+    }
+    return passOrFail;
+}
+
+bool textFileTest(){
+
+    bool passOrFail = false;
+    system("bin/tp2 --country can --output-format text --output-filename test.txt");
+    FILE *file = fopen("test.txt","r");
+    if(file != NULL){
+        printf("Test 'text' file réussi.\n");
+        passOrFail = true;
+        system("rm test.txt");
+    }else{
+        printf("Test 'text' file échoué.\n");
+    }
+    return passOrFail;
+}
 
 void test_jansson(void) {
     json_error_t error;
@@ -30,6 +61,24 @@ void test_afg(void) {
 }
 
 int main() {
+    printf("\nTests d'existence de fichiers.\n\n");
+    int pass = 0;
+    int fail = 0;
+    
+    if(dotFileTest()){
+        pass++;
+    }else{
+        fail++;
+    }
+
+    if(textFileTest()){
+        pass++;
+    }else{
+        fail++;
+    }
+
+    printf("%d tests réussis, %d échoués.\n\n\n", pass, fail); 
+
     CU_pSuite pSuite = NULL;
     // Initialisation
     if (CU_initialize_registry() != CUE_SUCCESS )
